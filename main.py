83ccf726b56976fc21f91eef06a6b96dbed3fa8f
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2025-02-07 22:09:23 +0000

    adds shot and asteroid collision check

--- main.py
+++ main.py
@@ -40,7 +40,11 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
-
+            
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    shot.kill()
+                    asteroid.kill()
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
